983ca923c31f4cb164ce4f2a90b125dd7c0b8732
AmandaGoncalves <amandagoncalveseng@gmail.com>
2020-09-04 16:00:33 -0300

    login page in dev

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import profile, register_game, update_game, delete_game
+from .views import profile, register_game, update_game, delete_game, login, sign_up, account_create
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', profile, name='profile'),
+    path('', login, name='login'),
     path('cadastro', register_game, name='register_game'),
     path('update/<int:id>/', update_game, name='update_game'),
     path('delete/<int:id>/', delete_game, name='delete_game'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('perfil', profile, name='profile'),
+    path('criarconta', sign_up, name='sign_up'),
+    path('contacriada', account_create, name='account_create')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Game
 from .forms import GameForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def profile(request):
@@ -37,5 +38,35 @@ def delete_game(request, id):
         return redirect('profile')
 
     return render(request, 'game-delete-ok.html', {'games': games})
+
+
+def account_create(request):
+    return render(request, 'sign_up.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('account_create')
+
+    else:
+        form = UserCreationForm()
+        return render(request, 'account.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('profile')
+
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+
+
 
 
